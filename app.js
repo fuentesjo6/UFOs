183ae38db28d8e70f11f7a5ac065c7eba1c057ea
Jonathan Fuentes <jonathanfuentes@Jonathans-MBP.fios-router.home>
2021-04-28 13:41:50 -0400

    HTML update

--- app.js
+++ app.js
@@ -42,3 +42,9 @@ function handleClick() {
         // just be the original tableData.
         buildTable(filteredData);
 };
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
